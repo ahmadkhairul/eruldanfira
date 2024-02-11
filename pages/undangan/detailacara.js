b75e1b04c49ac8ef47637989ec0f6697c80f76cf
KHAIRUL <tealkomdev-ahmad>
2024-02-11 22:31:11 +0700

    fix bug map

--- pages/undangan/detailacara.js
+++ pages/undangan/detailacara.js
@@ -16,17 +16,10 @@ const LocationMap = () => {
           Detail Acara
         </Animated>
         <Animated className="mapouter">
-          <div className="gmap_canvas">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d832.54359846814!2d107.7716220463638!3d-6.977780810278084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1703396215037!5m2!1sen!2sid"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            >
-            </iframe>
-            <br />
-          </div>
+          <a href="https://goo.gl/maps/bE73qygUydaAXJWC7" target="_blank">
+            <img src="/static/images/map.png" />
+          </a>
+          <br />
         </Animated>
         <div className="content">
           <Animated className="title">
@@ -35,7 +28,17 @@ const LocationMap = () => {
           <Animated className="deskripsi">
             Komplek Rancaekek Permai 2 Blok B22 No 12,
             Rt 1 Rw 23, Desa Jelegong, <br />
-            Kec. Rancaekek, Kab. Bandung
+            Kec. Rancaekek, Kab. Bandung <br />
+            <a href="https://goo.gl/maps/bE73qygUydaAXJWC7" target="_blank">
+              <p style={{
+                textDecorationLine: "underline",
+                textUnderlineOffset: "5px",
+                weight: "700",
+                fontSize: "18px"
+              }}>
+                detail lokasi click disini
+              </p>
+            </a>
           </Animated>
           <Animated className="title">
             Akad Nikah
